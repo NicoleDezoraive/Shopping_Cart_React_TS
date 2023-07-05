52ab1c0fb5477a9ae871ab9d9d27d588abb7cbd6
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2023-07-05 16:06:57 +0300

    add: deploy workflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route , useNavigate } from "react-router-dom"
 import { Container } from "react-bootstrap"
 import Home from "./pages/Home"
 import Store from "./pages/Store"
-import About from "./pages/About"
+// import About from "./pages/About"
 import Navbar from "./components/Navbar"
 import { ShoppingCartProvider } from "./context/ShoppingCartContext"
+import { useEffect } from "react"
+
 
 function App() {
+  
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/"); // Navigate to the home page
+  }, []);
+  
  return (
  <ShoppingCartProvider>
     <Navbar/>
     <Container className="mb-4">
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
+        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </Container>
  </ShoppingCartProvider>)
