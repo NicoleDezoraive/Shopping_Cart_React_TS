cc6c9b18ca03952a29b656e344bec86a2db5aa09
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2023-07-05 15:31:10 +0300

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-  }
-
-  if (command !== 'serve') {
-    config.base = '/SHOPPING_CART/'
-  }
-
-  return config
+export default defineConfig({ 
+  plugins: [react()],
+  base: '/SHOPPING_CART/',
 })
